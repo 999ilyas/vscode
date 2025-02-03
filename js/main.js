59dbd7b747cc1ffc4a98d9ae50ceb3584ecf58e9
Ilyas <69952569+999ilyas@users.noreply.github.com>
2025-02-03 17:01:44 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 		var isMobile = {Android: function() {return navigator.userAgent.match(/Android/i);},BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},Windows: function() {return navigator.userAgent.match(/IEMobile/i);},any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}};
 	function isIE() {
 		ua = navigator.userAgent;
-		var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+		var is_ie = ua.indexOf("MSIE ") > -1 ||gshshs ua.indexOf("Trident/") > -1;
 		return is_ie; 
 	}
 	if(isIE()){
